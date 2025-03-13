981c66eff1d2449c45b94921c5348c595d5b8e6a
jhulndev <github@sl.huln.dev>
2025-03-13 13:08:07 -0700

    docs(example): add version-selection example

---
+++ examples/version-selection/main.tf
@@ -0,0 +1,33 @@
+
+# Recommended: Select a specific version of Talos to use.
+module "lock_full_version" {
+  source = "../../"
+
+  talos_version_spec = {
+    number = "v1.9.4"
+  }
+  platform = "metal"
+}
+
+# Use a regex to lock the major.minor version to "1.8"
+# but allow any patch version.
+module "latest_stable_patch_version" {
+  source = "../../"
+
+  talos_version_spec = {
+    pattern = "v1\\.8\\..*"
+  }
+  platform = "metal"
+}
+
+# Use a regex pattern to select the latest version,
+# optionally allowing un-stable releases as well.
+module "latest_unstable_version" {
+  source = "../../"
+
+  talos_version_spec = {
+    pattern     = ".*"
+    stable_only = false
+  }
+  platform = "metal"
+}
---
+++ examples/version-selection/outputs.tf
@@ -0,0 +1,12 @@
+
+output "lock_full_version" {
+  value = module.lock_full_version
+}
+
+output "latest_stable_patch_version" {
+  value = module.latest_stable_patch_version
+}
+
+output "latest_unstable_version" {
+  value = module.latest_unstable_version
+}
